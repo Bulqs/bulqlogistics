49c124f3cf5ca22e3a1fbfa00049e387ef9e278b
stopbegging95 <stopbeging@gmail.com>
2024-09-26 11:51:32 +0100

    location page in progress updated

--- app/components/locationcard/LocationCard.tsx
+++ app/components/locationcard/LocationCard.tsx
@@ -4,23 +4,23 @@ const LocationCard: React.FC = () => {
   return (
     <div className='flex flex-col items-center justify-center w-4/12  px-4 py-8 bg-appTitleBgColor rounded-lg'>
       <div className="flex items-center justify-between px-3 w-full">
-        <h3 className='font-bold text-white'> Bulq Lagos </h3> <h4 className="bg-appTitleBgColor text-white flex items-center justify-center rounded-lg px-10 py-3"> Ikeja </h4>
+        <h3 className='font-bold text-white'> Bulq Lagos </h3> <h4 className="bg-appNav/60 text-white flex items-center justify-center rounded-lg px-10 py-3"> Ikeja </h4>
       </div>
       
       <div className="flex items-center justify-start gap-8 px-3 w-full mt-4">
-        <h4 className="bg-appTitleBgColor text-white flex items-center justify-center rounded-lg px-5 py-2"> Pick Up </h4>
-        <h4 className="bg-appTitleBgColor text-white flex items-center justify-center rounded-lg px-5 py-2"> Delivery </h4>
+        <h4 className="bg-appNav/60 text-white flex items-center justify-center rounded-lg px-5 py-2"> Pick Up </h4>
+        <h4 className="bg-appNav/60 text-white flex items-center justify-center rounded-lg px-5 py-2"> Delivery </h4>
       </div>
 
       <div className="flex text-white items-center justify-start px-3 w-full mt-4">
         <p> <span className="font-medium"> Address: {" "}</span> <span>123 IkeJa Road Lagos Island</span>  </p>
       </div>
 
-      <div className="flex flex-row w-full items-start justify-between bg-appNav/60  mt-6 px-2 py-1 rounded-md">
+      <div className="flex flex-row w-full items-start justify-between bg-appNav/60  mt-6 px-2 py-3 rounded-md">
         
         <div className="flex flex-col items-start justify-start w-full ">
-          <h4 className='text-white '> Working Hours</h4>
-          <div className='text-white flex flex-col items-start w-full'>
+          <h4 className='text-white bg-appTitleBgColor px-2 py-1 rounded-md'> Working Hours</h4>
+          <div className='text-white flex flex-col items-start w-full mt-4'>
             <p className='flex font-semibold items-start justify-between w-full'> <span className="font-medium"> Monday: {" "}</span> <span className=""> 8am - 5pm</span>  </p>
             <p className='flex font-semibold items-start justify-between w-full'> <span className="font-medium"> Tuesday: {" "}</span> <span> 8am - 5pm</span>  </p>
             <p className='flex font-semibold items-start justify-between w-full'> <span className="font-medium"> Wednesday: {" "}</span> <span> 8am - 5pm</span>  </p>
@@ -30,9 +30,9 @@ const LocationCard: React.FC = () => {
           </div>
         </div>
         
-        <div className="flex flex-col items-start justify-start w-full">
-          <h4 className='text-white flex flex-col items-end w-full'> Phone Number </h4>
-          <div className='text-white flex flex-col items-end w-full'>
+        <div className="flex flex-col items-end w-full">
+          <h4 className='text-white bg-appTitleBgColor rounded-md px-2 py-1 flex flex-col text-end'> Phone Number </h4>
+          <div className='text-white flex flex-col items-end w-full mt-4'>
             <p className='flex font-semibold'> <span className="font-medium"> +234 807 8789 675 </span>  </p>
             <p className='flex font-semibold'> <span className="font-medium"> +234 807 8789 675 </span>  </p>
             <p className='flex font-semibold'> <span className="font-medium"> +234 807 8789 675 </span>  </p>
--- app/location/page.tsx
+++ app/location/page.tsx
@@ -25,59 +25,63 @@ const page: React.FC = () => {
         backgroundImage={location}
       />
 
-      {/* Search input section goes here */}
-      <div className='w-full flex items-center justify-center px-8 py-8'>
+      <div className="container py-12">
+        {/* Search input section goes here */}
+        <div className='w-full flex items-center justify-center px-8 py-8'>
 
-        <div className=' flex p-2 w-9/12 items-center justify-center gap-4'>
-          <InputField
-            id="country"
-            name="country"
-            value={formData.country}
-            placeholder="Select Your Country"
-            dropdownOptions={['USA', 'Canada', 'UK', 'Nigeria']}
-            onChange={(e) => setFormData({ ...formData, country: e.target.value })}
-          />
+          <div className=' flex p-2 w-9/12 items-center justify-center gap-4'>
+            <InputField
+              id="country"
+              name="country"
+              value={formData.country}
+              placeholder="Select Your Country"
+              dropdownOptions={['USA', 'Canada', 'UK', 'Nigeria']}
+              onChange={(e) => setFormData({ ...formData, country: e.target.value })}
+            />
 
-          <InputField
-            id="state"
-            name="state"
-            value={formData.state}
-            placeholder="Select Your State"
-            dropdownOptions={['Lagos State', 'Akwa-Ibom State', 'Abuja', 'Oyo State', 'Nasarawa State']}
-            onChange={(e) => setFormData({ ...formData, state: e.target.value })}
-          />
+            <InputField
+              id="state"
+              name="state"
+              value={formData.state}
+              placeholder="Select Your State"
+              dropdownOptions={['Lagos State', 'Akwa-Ibom State', 'Abuja', 'Oyo State', 'Nasarawa State']}
+              onChange={(e) => setFormData({ ...formData, state: e.target.value })}
+            />
 
-          <InputField
-            id="city"
-            name="city"
-            value={formData.city}
-            placeholder="Select Your City"
-            dropdownOptions={['Ikorody', 'Abule Egba', 'Lekki']}
-            onChange={(e) => setFormData({ ...formData, city: e.target.value })}
-          />
+            <InputField
+              id="city"
+              name="city"
+              value={formData.city}
+              placeholder="Select Your City"
+              dropdownOptions={['Ikorody', 'Abule Egba', 'Lekki']}
+              onChange={(e) => setFormData({ ...formData, city: e.target.value })}
+            />
+
+          </div>
+
+          <div className=' flex w-3/12 h-5 p-2 items-center gap-2 justify-end'>
+
+            <Button className='flex'>
+              SEARCH
+            </Button>
+
+            <Link href="/" className="text-white flex bg-green-900 p-2 rounded-md">
+              Show All Delivery Hub
+            </Link>
+
+          </div>
 
         </div>
+        {/* Search input section ends here */}
 
-        <div className=' flex w-3/12 h-5 p-2 items-center gap-2 justify-center'>
-
-          <Button className='flex'>
-            SEARCH
-          </Button>
-
-          <Link href="/" className="text-white flex bg-green-900 p-2 rounded-md">
-            Show All Delivery Hub
-          </Link>
-
+        {/* Location Card Section Goes Here */}
+        <div className="flex items-center justify-between gap-6 px-8 py-8">
+          <LocationCard />
+          <LocationCard />
+          <LocationCard />
         </div>
-
+        {/* Location Card Section Ends Here */}
       </div>
-      {/* Search input section ends here */}
-
-      {/* Location Card Section Goes Here */}
-      <div className="flex items-center p-4 ">
-        <LocationCard />
-      </div>
-      {/* Location Card Section Ends Here */}
 
     </div>
   );
